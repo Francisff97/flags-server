20a87e0b0f193c693a7c640d5f80bce9cc62830b
Francisff97 <67428751+Francisff97@users.noreply.github.com>
2025-10-06 13:19:27 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,15 @@
+// next.config.js
+const path = require('path');
+
+const nextConfig = {
+  experimental: {
+    tsconfigPaths: true, // aiuta Next a leggere i paths del tsconfig
+  },
+  webpack: (config) => {
+    // forza @ a puntare alla root del repo
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+};
+
+module.exports = nextConfig;
